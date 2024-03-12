d0c27050d5f7eadf70e10d151cf93a6f9f44a271
Floris Halleriet <floris.halleriet@student.graafschapcollege.nl>
2024-03-12 12:51:52 +0100

    change db info to my own

--- Index.php
+++ Index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
 </head>
+
 <body>
-    <h1><?php echo "Website Users" ?></h1>
+    <h1>
+        <?php echo "Website Users" ?>
+    </h1>
     <form action="Insert.php" method="POST">
         <label for="fname">First name:</label><br>
         <input type="text" id="fname" name="fname"><br>
@@ -18,4 +22,5 @@
         <input type="reset" value="Reset">
     </form>
 </body>
+
 </html>
